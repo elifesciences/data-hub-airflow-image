26775f5a20bc94db7d404e082a04ae35d2322cc9
michael <m.owonibi@elifesciences.org>
2019-12-16 10:29:14 +0000

    #5271
    pr based changes to filenames

--- clone.sh
+++ clone.sh
@@ -3,20 +3,18 @@
 REPO_FILE=$1
 MAIN_GIT_REPO_DIR=$2
 
-
 if [ ! -d $MAIN_GIT_REPO_DIR ]; then
-      mkdir -p $MAIN_GIT_REPO_DIR
+    mkdir -p $MAIN_GIT_REPO_DIR
 fi
 
 rm -rf $MAIN_GIT_REPO_DIR\/*
 
-
 for row in $(jq -c '.[]' $REPO_FILE ); do
     _jq() {
-     echo ${row} | jq -r ${1}
+        echo ${row} | jq -r ${1}
     }
-  git clone $(_jq '.git_repo_url') "$MAIN_GIT_REPO_DIR/$(_jq '.clone_directory')"
-  cd "$MAIN_GIT_REPO_DIR/$(_jq '.clone_directory')"
+    git clone $(_jq '.git_repo_url') "$MAIN_GIT_REPO_DIR/$(_jq '.clone_directory')"
+    cd "$MAIN_GIT_REPO_DIR/$(_jq '.clone_directory')"
     git reset --hard $(_jq '.reference')
     cd ../../
 done
