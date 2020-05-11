60cb11ddfe0de1ad0741cb220dba1946edf61abd
michael <m.owonibi@elifesciences.org>
2020-05-11 09:26:43 +0100

    feat(data-science-dag): Add data science dag repo
    Add data science git repo to the repo list

--- scripts/install_dag_in_docker.sh
+++ scripts/install_dag_in_docker.sh
@@ -4,16 +4,21 @@ set -e
 
 DAGS_PATH=$AIRFLOW_HOME/dags
 REPOS_DIR=$AIRFLOW_HOME/git_repos
+APP_FILES_DIR=$AIRFLOW_HOME/applications_file_directory
 
 if [ ! -d $DAGS_PATH ]; then
   mkdir -p $DAGS_PATH
+fi
+
+if [ ! -d APP_FILES_DIR ]; then
+  mkdir -p APP_FILES_DIR
 fi
 
 cd $REPOS_DIR
 
 for dag_dir in */ ; do
   if [ -f $dag_dir/install.sh ]; then
-    $dag_dir/install.sh $DAGS_PATH
+    $dag_dir/install.sh $DAGS_PATH $APP_FILES_DIR
   elif [ -f $dag_dir/requirements.txt ]; then
     pip install --user -r $dag_dir/requirements.txt
     cp $dag_dir $DAGS_PATH -r
